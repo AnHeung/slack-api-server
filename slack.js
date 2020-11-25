576db9176cc73e9930c5c17a347c1a68da67fe00
gskebin <gskebin4@gmail.com>
2020-11-25 22:36:34 +0900

    마나토끼와 핫딜 전용 attachment 구분 작업

--- slack.js
+++ slack.js
@@ -20,13 +20,27 @@ const sendSlackMsg = async (type, crawlerData) => {
         }
     }
 
-    return await Axios.post(WEBHOOK_URL, JSON.stringify(configMessageBody(crawlerData)))
+    return await Axios.post(WEBHOOK_URL, JSON.stringify(configMessageBody(type, crawlerData)))
         .catch(err => {
             console.error(err)
+            return false
         })
 }
 
-function makeAttachment(crawlerData) {
+function makeAttachment(type, data) {
+    let attachment = [];
+    switch (type) {
+        case 'hotdeal':
+            attachment = makeHotDealAttachment(data)
+            break;
+        case 'manatoki':
+            attachment = makeMangaAttachment(data)
+            break;
+    }
+    return attachment;
+}
+
+function makeHotDealAttachment(crawlerData) {
 
     return crawlerData.reduce((acc, data) => {
 
@@ -68,9 +82,9 @@ function makeAttachment(crawlerData) {
     }, [])
 }
 
-function configMessageBody(crawlerData) {
+function configMessageBody(type, crawlerData) {
 
-    const attchment = makeAttachment(crawlerData)
+    const attchment = makeAttachment(type,crawlerData)
     const title = crawlerData.length > 0 ? crawlerData[0].title : "푸쉬알림"
 
     return {
@@ -78,6 +92,56 @@ function configMessageBody(crawlerData) {
         "attachments": attchment
     }
 }
+
+function makeMangaAttachment(mangaData) {
+
+    return mangaData.reduce((acc, data) => {
+
+        const { title, link, date, thumbnail, comicLink } = data
+
+        const attchment =  // attachments, here we also use long attachment to use more space
+        {
+            "color": "#2eb886",
+            "title_link": link,
+            "fields": [
+                {
+                    "title": "제목",
+                    "value": title,
+                    "short": false,
+                },
+                {
+                    "title": "업데이트 날짜",
+                    "value": date,
+                    "short": false,
+                },
+            ],
+            "thumb_url": thumbnail,
+            "actions": [ // Slack supports many kind of different types, we'll use buttons here
+                {
+                    "type": "button",
+                    "text": "보러가기", // text on the button 
+                    "url": link // url the button will take the user if clicked
+                },
+                {
+                    "type": "button",
+                    "text": "전편보기", // text on the button 
+                    "style": "danger",
+                    "url": comicLink // url the button will take the user if clicked
+                },
+                {
+                    "type": "button",
+                    "text": "다운로드", // text on the button 
+                    "style": "primary",
+                    "url": link // url the button will take the user if clicked
+                },
+            ]
+        }
+
+        acc.push(attchment)
+        return acc;
+    }, [])
+}
+
 
 module.exports = {
     sendSlackMsg: sendSlackMsg
