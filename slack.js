b829486d0017083147203387f0b116fc570e1a40
gskebin <gskebin4@gmail.com>
2021-02-01 11:36:07 +0900

    torrent update 관련 slack message 추가

--- slack.js
+++ slack.js
@@ -7,24 +7,30 @@ const Axios = require('axios');
 
 const sendSlackMsg = async (type, crawlerData) => {
 
-    let WEBHOOK_URL = process.env.SLACK_TEST_URL
+    let WEBHOOK_URL = process.env.NODE_ENV === 'prod' ? makeSlackUrl(type) : process.env.SLACK_TEST_URL
+    let messageBody = configMessageBody(type, crawlerData) || {}
 
-    if (process.env.NODE_ENV == 'prod') {
-        switch (type) {
-            case 'hotdeal':
-                WEBHOOK_URL = process.env.HOT_DEAL_SLACK_URL
-                break;
-            case 'manatoki':
-                WEBHOOK_URL = process.env.MANATOKI_SLACK_URL
-                break;
-        }
-    }
-
-    return await Axios.post(WEBHOOK_URL, JSON.stringify(configMessageBody(type, crawlerData)))
+    return await Axios.post(WEBHOOK_URL, JSON.stringify(messageBody))
         .catch(err => {
             console.error(`slackSend Message 오류 : ${err}`)
             return false
         })
+}
+
+function makeSlackUrl(type) {
+    let WEBHOOK_URL = SLACK_TEST_URL
+    switch (type) {
+        case 'hotdeal':
+            WEBHOOK_URL = process.env.HOT_DEAL_SLACK_URL
+            break;
+        case 'manatoki':
+            WEBHOOK_URL = process.env.MANATOKI_SLACK_URL
+            break;
+        case 'torrent_update':
+            WEBHOOK_URL = process.env.TORRENT_CRWALING_URL
+            break;
+    }
+    return WEBHOOK_URL
 }
 
 function makeAttachment(type, data) {
@@ -35,6 +41,9 @@ function makeAttachment(type, data) {
             break;
         case 'manatoki':
             attachment = makeMangaAttachment(data)
+            break;
+        case 'torrent_update':
+            attachment = makeTorrentUpdateAttachment(data)
             break;
     }
     return attachment;
@@ -82,9 +91,40 @@ function makeHotDealAttachment(crawlerData) {
     }, [])
 }
 
+function makeTorrentUpdateAttachment(crawlerData) {
+
+    return crawlerData.reduce((acc, data) => {
+
+        const { category, title, size , submitter, torrentLink, date } = data
+
+        const attchment =  // attachments, here we also use long attachment to use more space
+        {
+            "color": "#2eb886",
+            "fields": [
+                { "title": "사이트", "value": category , "short": false },
+                { "title": "제목", "value": title , "short": false },
+                { "title": "업데이트 날짜", "value": date, "short": false },
+                { "title": "자막자", "value": submitter, "short": false },
+                { "title": "파일 사이즈", "value": size, "short": false },
+            ],
+            "actions": [ // Slack supports many kind of different types, we'll use buttons here
+                {
+                    "type": "button",
+                    "text": "다운로드 링크", // text on the button 
+                    "style": "danger",
+                    "url": torrentLink // url the button will take the user if clicked
+                }
+            ]
+        }
+
+        acc.push(attchment)
+        return acc;
+    }, [])
+}
+
 function configMessageBody(type, crawlerData) {
 
-    const attchment = makeAttachment(type,crawlerData)
+    const attchment = makeAttachment(type, crawlerData)
     const title = crawlerData.length > 0 ? crawlerData[0].title : "푸쉬알림"
 
     return {
