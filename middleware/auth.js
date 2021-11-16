86c9894e450df86c8938a990c81a6d836bc0a24b
gskebin <gskebin4@gmail.com>
2021-11-16 17:22:26 +0900

    test

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ module.exports = (req, res, next) => {
   try {    
     const authKey = req.headers[process.env.API_KEY];
     console.error(`authKey ${authKey}`)
+    console.error(`process.env.API_KEY ${process.env.API_KEY}`)
     if (!authKey || authKey !== process.env.API_KEY_VALUE) {
       throw 'not matched key';
     } else {
