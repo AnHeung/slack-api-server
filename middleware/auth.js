0189ee90ed090dede0f4ae4224b796848365b41d
gskebin <gskebin4@gmail.com>
2021-11-16 18:09:38 +0900

    로그 삭제

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,6 @@ const {errMsg} = require('../errorHandle')
 module.exports = (req, res, next) => {
   try {    
     const authKey = req.headers[process.env.API_KEY];
-    console.error(`authKey ${authKey}`)
-    console.error(`process.env.API_KEY ${process.env.API_KEY}`)
-    console.error(`req.headers ${req.headers.common[process.env.API_KEY]}`)
     if (!authKey || authKey !== process.env.API_KEY_VALUE) {
       throw 'not matched key';
     } else {
