4934fb4c1c237ef7504dd7dc61fad653d55a1a1c
gskebin <gskebin4@gmail.com>
2021-11-16 16:22:42 +0900

    error 체크

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ const {errMsg} = require('../errorHandle')
 module.exports = (req, res, next) => {
   try {    
     const authKey = req.headers[process.env.API_KEY];
+    console.error(`authKey ${authKey}`)
     if (!authKey || authKey !== process.env.API_KEY_VALUE) {
       throw 'not matched key';
     } else {
